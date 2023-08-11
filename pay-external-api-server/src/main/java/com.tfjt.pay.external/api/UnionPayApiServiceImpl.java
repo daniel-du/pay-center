4f299db84c1c0e2c5a5399639cb157c45226f6c0
songxiaoxing <songxiaoxing613@163.com>
2023-08-11 11:34:15 +0800

    获取向同福转账的电子账簿接口 转账接口

---
+++ pay-external-api-server/src/main/java/com.tfjt.pay.external/api/UnionPayApiServiceImpl.java
@@ -0,0 +1,21 @@
+package com.tfjt.pay.external.api;
+
+import com.tfjt.pay.external.dto.UnionPayTransferDTO;
+import com.tfjt.tfcommon.dto.response.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboService;
+
+/**
+ * 银联接口服务实现类
+ * @author songx
+ * @date 2023-08-10 17:02
+ * @email 598482054@qq.com
+ */
+@Slf4j
+@DubboService
+public class UnionPayApiServiceImpl implements UnionPayApiService {
+    @Override
+    public Result<String> transfer(UnionPayTransferDTO payTransferDTO) {
+        return null;
+    }
+}
