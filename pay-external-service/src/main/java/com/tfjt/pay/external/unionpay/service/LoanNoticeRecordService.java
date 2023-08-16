21eddab2b84ad763a349a0124f1bb127af28358b
songxiaoxing <songxiaoxing613@163.com>
2023-08-16 17:17:44 +0800

    银联回调代码生成

--- pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/service/LoanNoticeRecordService.java
+++ pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/service/LoanNoticeRecordService.java
@@ -1,7 +1,6 @@
 package com.tfjt.pay.external.unionpay.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.tfjt.pay.external.unionpay.common.utils.PageUtils;
 import com.tfjt.pay.external.unionpay.entity.LoanNoticeRecordEntity;
 
 import java.util.Map;
@@ -15,6 +14,5 @@ import java.util.Map;
  */
 public interface LoanNoticeRecordService extends IService<LoanNoticeRecordEntity> {
 
-    PageUtils queryPage(Map<String, Object> params);
 }
 
--- pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/service/impl/LoanNoticeRecordServiceImpl.java
+++ pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/service/impl/LoanNoticeRecordServiceImpl.java
@@ -1,12 +1,7 @@
 package com.tfjt.pay.external.unionpay.service.impl;
 
 import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.tfjt.pay.external.unionpay.common.utils.PageUtils;
-import com.tfjt.pay.external.unionpay.common.utils.Query;
 
 import com.tfjt.pay.external.unionpay.dao.LoanNoticeRecordDao;
 import com.tfjt.pay.external.unionpay.entity.LoanNoticeRecordEntity;
@@ -15,15 +10,5 @@ import com.tfjt.pay.external.unionpay.service.LoanNoticeRecordService;
 
 @Service("loanNoticeRecordService")
 public class LoanNoticeRecordServiceImpl extends ServiceImpl<LoanNoticeRecordDao, LoanNoticeRecordEntity> implements LoanNoticeRecordService {
-
-    @Override
-    public PageUtils queryPage(Map<String, Object> params) {
-        IPage<LoanNoticeRecordEntity> page = this.page(
-                new Query<LoanNoticeRecordEntity>().getPage(params),
-                new QueryWrapper<LoanNoticeRecordEntity>()
-        );
-
-        return new PageUtils(page);
-    }
 
 }
