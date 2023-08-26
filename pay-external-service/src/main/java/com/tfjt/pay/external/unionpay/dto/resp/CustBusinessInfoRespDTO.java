f916cc09f600e3b98daa7901f7c29f37bf759cba
tony <blank.lee@hotmail.com>
2023-08-26 14:06:22 +0800

    修改图片信息逻辑

---
+++ pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/dto/resp/CustBusinessInfoRespDTO.java
@@ -0,0 +1,45 @@
+package com.tfjt.pay.external.unionpay.dto.resp;
+
+import lombok.Data;
+
+/**
+ * @author tony
+ * @version 1.0
+ * @title CustBusinessInfoReqDTO
+ * @description
+ * @create 2023/8/26 13:21
+ */
+@Data
+public class CustBusinessInfoRespDTO {
+    private Long id;
+    /**
+     * 是否有固定营业场所 0 无 1 有
+     */
+    private Integer isFixedLocation;
+    /**
+     * 门面照
+     */
+    private String facadePhotoUrl;
+    private Long facadePhotoId;
+
+    /**
+     * 店铺室内
+     */
+    private String shopPhotoUrl;
+    private Long shopPhotoId;
+
+
+    /**
+     * 商品照片
+     */
+    private String goodsPhotoUrl;
+    private Long goodsPhotoId;
+
+    /**
+     * 辅助正面材料
+     */
+    private String auxiliaryPhotoUrl;
+    private Long auxiliaryPhotoId;
+
+    private Long loanUserId;
+}
--- pay-external-web/src/main/java/com/tfjt/pay/external/unionpay/web/controller/CustBusinessInfoController.java
+++ pay-external-web/src/main/java/com/tfjt/pay/external/unionpay/web/controller/CustBusinessInfoController.java
@@ -2,6 +2,7 @@ package com.tfjt.pay.external.unionpay.web.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.tfjt.pay.external.unionpay.dto.resp.CustBusinessInfoRespDTO;
 import com.tfjt.pay.external.unionpay.service.CustBusinessInfoService;
 import com.tfjt.pay.external.unionpay.entity.CustBusinessAttachInfoEntity;
 import com.tfjt.pay.external.unionpay.entity.CustBusinessInfoEntity;
@@ -11,6 +12,7 @@ import com.tfjt.pay.external.unionpay.utils.DateUtil;
 import com.tfjt.tfcommon.dto.response.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -55,33 +57,33 @@ public class CustBusinessInfoController {
     public Result<?> info(@PathVariable("loanUserId") Integer loanUserId) {
         CustBusinessInfoEntity custBusinessInfo = custBusinessInfoService.getBaseMapper().selectOne(Wrappers.lambdaQuery(CustBusinessInfoEntity.class).eq(CustBusinessInfoEntity::getLoanUserId, loanUserId));
         List<CustBusinessAttachInfoEntity> attachInfoList = custBusinessAttachInfoService.list(Wrappers.lambdaQuery(CustBusinessAttachInfoEntity.class).eq(CustBusinessAttachInfoEntity::getCustBusinessInfoId, custBusinessInfo.getId()));
-        List<CustBusinessAttachInfoEntity> goodsAttachInfoList = new ArrayList<>();
         /**
          * 证明材料
          */
-        List<CustBusinessAttachInfoEntity> evidenceAttachInfoList = new ArrayList<>();
+        CustBusinessInfoRespDTO custBusinessInfoRespDTO = new CustBusinessInfoRespDTO();
+        BeanUtils.copyProperties(custBusinessInfo, custBusinessInfoRespDTO);
         attachInfoList.forEach(attachInfo -> {
             if (attachInfo.getType().equals(ImgTypeEnum.FACADE_PHOTO.getCode())) {
-                custBusinessInfo.setFacadePhotoUrl(attachInfo.getImgUrl());
-                custBusinessInfo.setFacadePhotoId(attachInfo.getId());
+                custBusinessInfoRespDTO.setFacadePhotoUrl(attachInfo.getImgUrl());
+                custBusinessInfoRespDTO.setFacadePhotoId(attachInfo.getId());
             }
 
             if (attachInfo.getType().equals(ImgTypeEnum.SHOP_PHOTO.getCode())) {
-                custBusinessInfo.setShopPhotoUrl(attachInfo.getImgUrl());
-                custBusinessInfo.setShopPhotoId(attachInfo.getId());
+                custBusinessInfoRespDTO.setShopPhotoUrl(attachInfo.getImgUrl());
+                custBusinessInfoRespDTO.setShopPhotoId(attachInfo.getId());
             }
 
             if (attachInfo.getType().equals(ImgTypeEnum.GOOD_PHOTO.getCode())) {
-                goodsAttachInfoList.add(attachInfo);
+                custBusinessInfoRespDTO.setGoodsPhotoUrl(attachInfo.getImgUrl());
+                custBusinessInfoRespDTO.setGoodsPhotoId(attachInfo.getId());
             }
 
             if (attachInfo.getType().equals(ImgTypeEnum.EVIDENC_PHOTO.getCode())) {
-                evidenceAttachInfoList.add(attachInfo);
+                custBusinessInfoRespDTO.setAuxiliaryPhotoUrl(attachInfo.getImgUrl());
+                custBusinessInfoRespDTO.setAuxiliaryPhotoId(attachInfo.getId());
             }
         });
-        custBusinessInfo.setGoodsAttachInfoList(goodsAttachInfoList);
-        custBusinessInfo.setEvidenceAttachInfoList(evidenceAttachInfoList);
-        return Result.ok(custBusinessInfo);
+        return Result.ok(custBusinessInfoRespDTO);
     }
 
     /**
@@ -89,13 +91,15 @@ public class CustBusinessInfoController {
      */
     @RequestMapping("/save")
     @Transactional(rollbackFor = Exception.class)
-    public Result<?> save(@RequestBody CustBusinessInfoEntity custBusinessInfo) {
+    public Result<?> save(@RequestBody CustBusinessInfoRespDTO custBusinessInfo) {
+        CustBusinessInfoEntity custBusinessInfoEntity = new CustBusinessInfoEntity();
         try {
             boolean flag = DateUtil.timeComparison(null,null);
             if(!flag){
                 return Result.failed("0点到凌晨04点，不受理申请！");
             }
-            custBusinessInfoService.save(custBusinessInfo);
+            BeanUtils.copyProperties(custBusinessInfo, custBusinessInfoEntity);
+            custBusinessInfoService.save(custBusinessInfoEntity);
         } catch (Exception ex) {
             log.error("保存异常", ex);
             return Result.failed("保存异常");
@@ -107,28 +111,28 @@ public class CustBusinessInfoController {
          */
         CustBusinessAttachInfoEntity facadeBusinessAttachInfo = new CustBusinessAttachInfoEntity();
         facadeBusinessAttachInfo.setType(ImgTypeEnum.FACADE_PHOTO.getCode());
-        facadeBusinessAttachInfo.setCustBusinessInfoId(custBusinessInfo.getId());
+        facadeBusinessAttachInfo.setCustBusinessInfoId(custBusinessInfoEntity.getId());
         facadeBusinessAttachInfo.setImgUrl(custBusinessInfo.getFacadePhotoUrl());
+
         CustBusinessAttachInfoEntity shopBusinessAttachInfo = new CustBusinessAttachInfoEntity();
         shopBusinessAttachInfo.setType(ImgTypeEnum.SHOP_PHOTO.getCode());
-        shopBusinessAttachInfo.setCustBusinessInfoId(custBusinessInfo.getId());
+        shopBusinessAttachInfo.setCustBusinessInfoId(custBusinessInfoEntity.getId());
         shopBusinessAttachInfo.setImgUrl(custBusinessInfo.getShopPhotoUrl());
 
+        CustBusinessAttachInfoEntity goodsAttachInfo = new CustBusinessAttachInfoEntity();
+        goodsAttachInfo.setType(ImgTypeEnum.GOOD_PHOTO.getCode());
+        goodsAttachInfo.setCustBusinessInfoId(custBusinessInfoEntity.getId());
+        goodsAttachInfo.setImgUrl(custBusinessInfo.getGoodsPhotoUrl());
+
+        CustBusinessAttachInfoEntity evidenceAttachInfo = new CustBusinessAttachInfoEntity();
+        evidenceAttachInfo.setType(ImgTypeEnum.EVIDENC_PHOTO.getCode());
+        evidenceAttachInfo.setCustBusinessInfoId(custBusinessInfoEntity.getId());
+        evidenceAttachInfo.setImgUrl(custBusinessInfo.getAuxiliaryPhotoUrl());
 
         businessAttachInfo.add(facadeBusinessAttachInfo);
         businessAttachInfo.add(shopBusinessAttachInfo);
-        custBusinessInfo.getGoodsAttachInfoList().forEach(custBusinessAttachInfoEntity -> {
-            custBusinessAttachInfoEntity.setType(ImgTypeEnum.GOOD_PHOTO.getCode());
-            custBusinessAttachInfoEntity.setCustBusinessInfoId(custBusinessInfo.getId());
-        });
-        custBusinessInfo.getEvidenceAttachInfoList().forEach(custBusinessAttachInfoEntity -> {
-            custBusinessAttachInfoEntity.setType(ImgTypeEnum.EVIDENC_PHOTO.getCode());
-            custBusinessAttachInfoEntity.setCustBusinessInfoId(custBusinessInfo.getId());
-        });
-
-        businessAttachInfo.addAll(custBusinessInfo.getGoodsAttachInfoList());
-        businessAttachInfo.addAll(custBusinessInfo.getEvidenceAttachInfoList());
-
+        businessAttachInfo.add(goodsAttachInfo);
+        businessAttachInfo.add(evidenceAttachInfo);
         try {
             custBusinessAttachInfoService.saveBatch(businessAttachInfo);
         } catch (Exception ex) {
@@ -143,38 +147,38 @@ public class CustBusinessInfoController {
      */
     @RequestMapping("/update")
     @Transactional(rollbackFor = Exception.class)
-    public Result<?> update(@RequestBody CustBusinessInfoEntity custBusinessInfo) {
+    public Result<?> update(@RequestBody CustBusinessInfoRespDTO custBusinessInfo) {
         try {
-            boolean bool = custBusinessInfoService.updateById(custBusinessInfo);
-            custBusinessInfo.getGoodsAttachInfoList().forEach(custBusinessAttachInfoEntity -> {
-                if (custBusinessAttachInfoEntity.getId() != null) {
-                    custBusinessAttachInfoService.updateById(custBusinessAttachInfoEntity);
-                } else {
-                    custBusinessAttachInfoEntity.setCustBusinessInfoId(custBusinessInfo.getId());
-                    custBusinessAttachInfoService.save(custBusinessAttachInfoEntity);
-                }
-            });
-
-            custBusinessInfo.getEvidenceAttachInfoList().forEach(custBusinessAttachInfoEntity -> {
-                if (custBusinessAttachInfoEntity.getId() != null) {
-                    custBusinessAttachInfoService.updateById(custBusinessAttachInfoEntity);
-                } else {
-                    custBusinessAttachInfoEntity.setCustBusinessInfoId(custBusinessInfo.getId());
-                    custBusinessAttachInfoService.save(custBusinessAttachInfoEntity);
-                }
-            });
+            CustBusinessInfoEntity custBusinessInfoEntity = new CustBusinessInfoEntity();
+            BeanUtils.copyProperties(custBusinessInfo, custBusinessInfoEntity);
+            boolean bool = custBusinessInfoService.updateById(custBusinessInfoEntity);
 
             if(StringUtils.isNotBlank(custBusinessInfo.getFacadePhotoUrl())){
-                CustBusinessAttachInfoEntity custBusinessAttachInfoEntity = custBusinessAttachInfoService.getOne(new LambdaQueryWrapper<CustBusinessAttachInfoEntity>().eq(CustBusinessAttachInfoEntity::getCustBusinessInfoId, custBusinessInfo.getId()).eq(CustBusinessAttachInfoEntity::getType, 1));
+                CustBusinessAttachInfoEntity custBusinessAttachInfoEntity = custBusinessAttachInfoService.getById(custBusinessInfo.getFacadePhotoId());
                 if(custBusinessAttachInfoEntity !=null ){
                     custBusinessAttachInfoEntity.setImgUrl(custBusinessInfo.getFacadePhotoUrl());
                     custBusinessAttachInfoService.updateById(custBusinessAttachInfoEntity);
                 }
             }
             if(StringUtils.isNotBlank(custBusinessInfo.getShopPhotoUrl())){
-                CustBusinessAttachInfoEntity custBusinessAttachInfoEntity = custBusinessAttachInfoService.getOne(new LambdaQueryWrapper<CustBusinessAttachInfoEntity>().eq(CustBusinessAttachInfoEntity::getCustBusinessInfoId, custBusinessInfo.getId()).eq(CustBusinessAttachInfoEntity::getType, 2));
+                CustBusinessAttachInfoEntity custBusinessAttachInfoEntity = custBusinessAttachInfoService.getById(custBusinessInfo.getShopPhotoId());
                 if(custBusinessAttachInfoEntity !=null ){
                     custBusinessAttachInfoEntity.setImgUrl(custBusinessInfo.getShopPhotoUrl());
+                    custBusinessAttachInfoService.updateById(custBusinessAttachInfoEntity);
+                }
+            }
+
+            if(StringUtils.isNotBlank(custBusinessInfo.getGoodsPhotoUrl())){
+                CustBusinessAttachInfoEntity custBusinessAttachInfoEntity = custBusinessAttachInfoService.getById(custBusinessInfo.getGoodsPhotoId());
+                if(custBusinessAttachInfoEntity !=null ){
+                    custBusinessAttachInfoEntity.setImgUrl(custBusinessInfo.getGoodsPhotoUrl());
+                    custBusinessAttachInfoService.updateById(custBusinessAttachInfoEntity);
+                }
+            }
+            if(StringUtils.isNotBlank(custBusinessInfo.getAuxiliaryPhotoUrl())){
+                CustBusinessAttachInfoEntity custBusinessAttachInfoEntity = custBusinessAttachInfoService.getById(custBusinessInfo.getAuxiliaryPhotoId());
+                if(custBusinessAttachInfoEntity !=null ){
+                    custBusinessAttachInfoEntity.setImgUrl(custBusinessInfo.getAuxiliaryPhotoUrl());
                     custBusinessAttachInfoService.updateById(custBusinessAttachInfoEntity);
                 }
             }
