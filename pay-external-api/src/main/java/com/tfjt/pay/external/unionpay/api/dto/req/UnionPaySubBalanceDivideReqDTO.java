30453417d60e18618545aeff3326e617974a5697
songxiaoxing <songxiaoxing613@163.com>
2023-08-26 14:09:49 +0800

    回调增加字段

--- pay-external-api/src/main/java/com/tfjt/pay/external/unionpay/api/dto/req/UnionPaySubBalanceDivideReqDTO.java
+++ pay-external-api/src/main/java/com/tfjt/pay/external/unionpay/api/dto/req/UnionPaySubBalanceDivideReqDTO.java
@@ -2,6 +2,8 @@ package com.tfjt.pay.external.unionpay.api.dto.req;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
@@ -15,12 +17,17 @@ public class UnionPaySubBalanceDivideReqDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**收款电子账簿id*/
+    @NotNull(message = "收款电子账簿id不能为空")
     private String recvBalanceAcctId;
     /**收款户名*/
+    @NotNull(message = "收款户名不能为空")
     private String recvBalanceAcctName;
     /**交易金额*/
+    @NotNull(message = "交易金额不能为空")
+    @Min(message = "交易金额不能小于一分",value = 1)
     private Integer amount;
     /**业务系统订单*/
+    @NotNull(message = "业务系统订单号不能为空")
     private String subBusinessOrderNo;
     /**交易附言*/
     private String createRemark;
