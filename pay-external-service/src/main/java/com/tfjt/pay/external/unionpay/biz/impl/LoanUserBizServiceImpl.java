0c7986a01e6bdcccf2dffa0884f7e5ad64553fd5
zhs <zhs>
2023-10-21 17:21:58 +0800

    1.修改商户设置密码逻辑

--- pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/biz/impl/LoanUserBizServiceImpl.java
+++ pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/biz/impl/LoanUserBizServiceImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollectionUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.lock.annotation.Lock4j;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
@@ -70,14 +71,22 @@ public class LoanUserBizServiceImpl implements LoanUserBizService {
     }
 
     @Override
-    @Lock4j(keys = {"#paymentPasswordDTO.busId","#paymentPasswordDTO.type","#paymentPasswordDTO.password"}, expire = 3000, acquireTimeout = 4000)
+    @Lock4j(keys = {"#paymentPasswordDTO.busId", "#paymentPasswordDTO.type", "#paymentPasswordDTO.password"}, expire = 3000, acquireTimeout = 4000)
     public Result<String> savePaymentPassword(PaymentPasswordReqDTO paymentPasswordDTO) {
         checkLoanUser(paymentPasswordDTO.getBusId(), paymentPasswordDTO.getType());
         try {
             ValidatorUtils.validateEntity(paymentPasswordDTO);
             PaymentPasswordEntity paymentPassword = new PaymentPasswordEntity();
             BeanUtils.copyProperties(paymentPasswordDTO, paymentPassword);
-            paymentPasswordService.save(paymentPassword);
+            //验证此用户是否有支付密码
+            PaymentPasswordEntity ppeEntity = paymentPasswordService.getOne(new LambdaQueryWrapper<PaymentPasswordEntity>()
+                    .eq(PaymentPasswordEntity::getType, paymentPasswordDTO.getType())
+                    .eq(PaymentPasswordEntity::getBusId, paymentPasswordDTO.getBusId()));
+            if (ppeEntity == null) {
+                paymentPasswordService.save(paymentPassword);
+            } else {
+                this.updatePaymentPassword(paymentPasswordDTO);
+            }
         } catch (Exception ex) {
             if (ex instanceof DuplicateKeyException) {
                 return Result.failed(PayExceptionCodeEnum.REPEAT_OPERATION.getMsg());
@@ -89,7 +98,7 @@ public class LoanUserBizServiceImpl implements LoanUserBizService {
     }
 
     @Override
-    @Lock4j(keys = {"#paymentPasswordDTO.busId","#paymentPasswordDTO.type","#paymentPasswordDTO.password"}, expire = 3000, acquireTimeout = 4000)
+    @Lock4j(keys = {"#paymentPasswordDTO.busId", "#paymentPasswordDTO.type", "#paymentPasswordDTO.password"}, expire = 3000, acquireTimeout = 4000)
     public Result<String> updatePaymentPassword(PaymentPasswordReqDTO paymentPasswordDTO) {
         checkLoanUser(paymentPasswordDTO.getBusId(), paymentPasswordDTO.getType());
         try {
@@ -194,7 +203,7 @@ public class LoanUserBizServiceImpl implements LoanUserBizService {
 
     @Override
     public Result<LoanTransferToTfRespDTO> getBalanceAcctId(String type, String bid) {
-        if(StringUtil.isBlank(type) || StringUtil.isBlank(bid)){
+        if (StringUtil.isBlank(type) || StringUtil.isBlank(bid)) {
             return Result.failed(PayExceptionCodeEnum.TREAD_PARAMETER_ILLEGAL);
         }
         LoanTransferToTfRespDTO loanTransferToTfDTO = new LoanTransferToTfRespDTO();
@@ -212,7 +221,7 @@ public class LoanUserBizServiceImpl implements LoanUserBizService {
 
     @Override
     public Result<Map<String, Object>> incomingIsFinish(String type, String bid) {
-        if(StringUtil.isBlank(type) || StringUtil.isBlank(bid)){
+        if (StringUtil.isBlank(type) || StringUtil.isBlank(bid)) {
             return Result.failed(PayExceptionCodeEnum.TREAD_PARAMETER_ILLEGAL);
         }
         Map<String, Object> result = new HashMap<>();
@@ -245,7 +254,7 @@ public class LoanUserBizServiceImpl implements LoanUserBizService {
     @Override
     public Result<Map<String, Object>> listIncomingIsFinish(List<UnionPayIncomingDTO> list) {
         log.info("listIncomingIsFinish 入参:{}", JSONObject.toJSONString(list));
-        if (CollectionUtil.isEmpty(list)){
+        if (CollectionUtil.isEmpty(list)) {
             return Result.failed(PayExceptionCodeEnum.TREAD_PARAMETER_ILLEGAL);
         }
         try {
@@ -264,27 +273,27 @@ public class LoanUserBizServiceImpl implements LoanUserBizService {
             UnionPayIncomingDTO unionPayIncomingDTO = shops.get(NumberConstant.ZERO);
             Map<String, Object> returnMap = new HashMap<>();
             Result<Map<String, Object>> map = incomingIsFinish(unionPayIncomingDTO.getType(), unionPayIncomingDTO.getBid());
-            if(map.getCode()!=NumberConstant.ZERO){
+            if (map.getCode() != NumberConstant.ZERO) {
                 return Result.failed(map.getMsg());
             }
-            returnMap.put("supplierFrozen",false);
-            returnMap.put("supplierIncoming",true);
-            returnMap.put("shopFrozen",map.getData().get("isFrozen"));
-            returnMap.put("shopIncoming",map.getData().get("isIncoming"));
-            returnMap.put("shopSettledAmount",map.getData().get("settledAmount"));
+            returnMap.put("supplierFrozen", false);
+            returnMap.put("supplierIncoming", true);
+            returnMap.put("shopFrozen", map.getData().get("isFrozen"));
+            returnMap.put("shopIncoming", map.getData().get("isIncoming"));
+            returnMap.put("shopSettledAmount", map.getData().get("settledAmount"));
             for (UnionPayIncomingDTO unionPayIncoming : dealers) {
                 Result<Map<String, Object>> mapResult = incomingIsFinish(unionPayIncoming.getType(), unionPayIncoming.getBid());
                 if (mapResult.getCode() == NumberConstant.ZERO) {
                     Map<String, Object> data = mapResult.getData();
-                    if(!Boolean.parseBoolean(data.get("isIncoming").toString())){
-                        returnMap.put("supplierIncoming",false);
+                    if (!Boolean.parseBoolean(data.get("isIncoming").toString())) {
+                        returnMap.put("supplierIncoming", false);
                         return Result.ok(returnMap);
                     }
-                    if(Boolean.parseBoolean(data.get("isFrozen").toString())){
-                        returnMap.put("supplierFrozen",true);
+                    if (Boolean.parseBoolean(data.get("isFrozen").toString())) {
+                        returnMap.put("supplierFrozen", true);
                         return Result.ok(returnMap);
                     }
-                }else{
+                } else {
                     return Result.failed(mapResult.getMsg());
                 }
             }
