bf48347e9d1fa077a74bdef9c169c5269d91797d
songxiaoxing <songxiaoxing613@163.com>
2024-02-05 09:44:23 +0800

    银行名称修改

--- pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/enums/digital/DigitalBankCodeEnum.java
+++ pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/enums/digital/DigitalBankCodeEnum.java
@@ -11,25 +11,25 @@ import lombok.Getter;
 public enum DigitalBankCodeEnum {
 
     /**工行*/
-    DCRI_ICBC("DCRI_ICBC","工行","https://qiniu.tongfuyouxuan.com/shop/240124102947-RAHRUr.png"),
+    DCRI_ICBC("DCRI_ICBC","工商银行","https://qiniu.tongfuyouxuan.com/shop/240124102947-RAHRUr.png"),
     /**农行*/
-    DCRI_ABC("DCRI_ABC","农行","https://qiniu.tongfuyouxuan.com/shop/240124102823-a8o6K8.png"),
+    DCRI_ABC("DCRI_ABC","农业银行","https://qiniu.tongfuyouxuan.com/shop/240124102823-a8o6K8.png"),
     /**中行*/
-    DCRI_BOC("DCRI_BOC","中行","https://qiniu.tongfuyouxuan.com/shop/231205141354-ohk3UC.png"),
+    DCRI_BOC("DCRI_BOC","中国银行","https://qiniu.tongfuyouxuan.com/shop/231205141354-ohk3UC.png"),
     /**建行*/
-    DCRI_CCB("DCRI_CCB","建行","https://qiniu.tongfuyouxuan.com/shop/240124102844-F4JQYs.png"),
+    DCRI_CCB("DCRI_CCB","建设银行","https://qiniu.tongfuyouxuan.com/shop/240124102844-F4JQYs.png"),
     /**交行*/
-    DCRI_BCM("DCRI_BCM","交行","https://qiniu.tongfuyouxuan.com/shop/240124103011-7Y9K23.png"),
+    DCRI_BCM("DCRI_BCM","交通银行","https://qiniu.tongfuyouxuan.com/shop/240124103011-7Y9K23.png"),
     /**邮储*/
-    DCRI_PSBC("DCRI_PSBC","邮储","https://qiniu.tongfuyouxuan.com/shop/240124103006-IiEq3m.png"),
+    DCRI_PSBC("DCRI_PSBC","邮储银行","https://qiniu.tongfuyouxuan.com/shop/240124103006-IiEq3m.png"),
     /**网商*/
-    DCRI_MYBANK("DCRI_MYBANK","网商","https://qiniu.tongfuyouxuan.com/shop/240124103041-JITwQp.png"),
+    DCRI_MYBANK("DCRI_MYBANK","网商银行","https://qiniu.tongfuyouxuan.com/shop/240124103041-JITwQp.png"),
     /**微众*/
-    DCRI_WEBANK("DCRI_WEBANK","微众","https://qiniu.tongfuyouxuan.com/shop/240124103037-JhwDce.png"),
+    DCRI_WEBANK("DCRI_WEBANK","微众银行","https://qiniu.tongfuyouxuan.com/shop/240124103037-JhwDce.png"),
     /**招行*/
-    DCRI_CMB("DCRI_CMB","招行","https://qiniu.tongfuyouxuan.com/shop/240124102802-zVxh2s.png"),
+    DCRI_CMB("DCRI_CMB","招行银行","https://qiniu.tongfuyouxuan.com/shop/240124102802-zVxh2s.png"),
     /**兴业*/
-    DCRI_CIB("DCRI_CIB","兴业","https://qiniu.tongfuyouxuan.com/shop/240124102707-qHHigZ.png");
+    DCRI_CIB("DCRI_CIB","兴业银行","https://qiniu.tongfuyouxuan.com/shop/240124102707-qHHigZ.png");
     private String code;
 
     private String desc;
