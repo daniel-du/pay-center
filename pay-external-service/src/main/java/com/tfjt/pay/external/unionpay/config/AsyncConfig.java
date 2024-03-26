77f76aed5265e0572090b0df2aeabdc9efd2022f
dupl <18234048366@139.com>
2024-03-26 20:29:19 +0800

    批量查询是否平安入网使用缓存

---
+++ pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/config/AsyncConfig.java
@@ -0,0 +1,33 @@
+package com.tfjt.pay.external.unionpay.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author Du Penglun
+ * @version 1.0
+ * @date 2024/3/26 20:21
+ * @description
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    @Bean
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // 核心线程数
+        executor.setMaxPoolSize(10); // 最大线程数
+        executor.setQueueCapacity(Integer.MAX_VALUE); // 队列大小
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+        executor.initialize();
+        return executor;
+    }
+}
