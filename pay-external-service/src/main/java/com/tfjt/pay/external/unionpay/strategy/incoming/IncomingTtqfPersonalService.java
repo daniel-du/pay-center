328179c58f9841eaffa1cfc4269a8bc4c262fea0
dupl <18234048366@139.com>
2024-03-29 16:16:16 +0800

    天天企赋回调逻辑修复

--- pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/strategy/incoming/IncomingTtqfPersonalService.java
+++ pay-external-service/src/main/java/com/tfjt/pay/external/unionpay/strategy/incoming/IncomingTtqfPersonalService.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -49,10 +50,14 @@ public class IncomingTtqfPersonalService extends AbstractIncomingService {
             String expiryStartStr = format2.format(expiryStart);
             Date expiryEnd = format1.parse(incomingSubmitMessageDTO.getLegalIdExpiryEnd());
             String expiryEndStr = format2.format(expiryEnd);
+            StopWatch sw = new StopWatch();
+            sw.start();
             //上传身份证正面照片
             String frontFieldId = TtqfApiUtil.pictureUpload(incomingSubmitMessageDTO.getLegalIdFrontUrl());
+            log.info("IncomingTtqfPersonalService--frontFieldIdSW:{}",  sw.getLastTaskTimeMillis());
             //上传身份证背面照片
             String backFieldId = TtqfApiUtil.pictureUpload(incomingSubmitMessageDTO.getLegalIdBackUrl());
+            log.info("IncomingTtqfPersonalService--backFieldIdSW:{}",  sw.getLastTaskTimeMillis());
             PresignModel model = PresignModel.builder().name(incomingSubmitMessageDTO.getLegalName())
                     .idCardNo(incomingSubmitMessageDTO.getLegalIdNo())
                     .mobile(incomingSubmitMessageDTO.getLegalMobile())
@@ -63,6 +68,8 @@ public class IncomingTtqfPersonalService extends AbstractIncomingService {
                     .idCardPicBFileId(backFieldId)
                     .notifyUrl(notifyUrl).build();
             PresignResultModel resultModel = TtqfApiUtil.presign(model);
+            sw.stop();
+            log.info("IncomingTtqfPersonalService--presignSW:{}",  sw.getLastTaskTimeMillis());
             TfIncomingExtendInfoEntity extendInfo = new TfIncomingExtendInfoEntity();
             extendInfo.setIncomingId(incomingSubmitMessageDTO.getIncomingId());
             extendInfo.setAuthStatus(resultModel.getAuthStatus().byteValue());
